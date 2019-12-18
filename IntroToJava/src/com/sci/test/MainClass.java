89727050b9833977e68a8115907a72ca68207336
cosmina <cosmina_elena18@yaho..com>
2019-12-18 19:45:03 +0200

    insert another message

---
+++ IntroToJava/src/com/sci/test/MainClass.java
@@ -0,0 +1,9 @@
+package com.sci.test;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from my subproject");
+        System.out.println("Test");
+    }
+}
